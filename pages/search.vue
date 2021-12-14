eedfb7ac182536650b880001d6adfe364f04cd58
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2021-12-14 09:44:54 -0800

    facets working, needs style

--- pages/search.vue
+++ pages/search.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="pages">
-    <ul>
-      <li v-for="(tag, index) in facets" :key="index">
-        <input
-          type="checkbox"
-          :value="tag.id"
-          @change="filter(tag.id)"
-        />{{ tag.slug }}
+    <ul class="flex flex-wrap">
+      <li class="w-1/3" v-for="(tag, index) in facets" :key="index">
+        <input type="checkbox" :value="tag.id" @change="filter(tag.id)" />{{
+          tag.slug
+        }}
       </li>
     </ul>
-    <ul class="flex flex-wrap p-8 search">
+    <ul v-if="filtered && filtered.length">
+      <li v-for="post in filtered" class="w-1/3 m-4 p-4">
+        <NuxtLink :to="post.link"
+          ><img class="thumb" :src="post.media"
+        /></NuxtLink>
+        <h3 class="text-xl">{{ post.title }}</h3>
+        <p class="text-md font-bold">{{ post.blurb }}</p>
+      </li>
+    </ul>
+    <ul v-else class="flex flex-wrap p-8 search">
       <li v-for="page in search" class="w-1/3 m-4 p-4">
         <NuxtLink :to="page.link"
           ><img class="thumb" :src="page.media"
@@ -22,10 +29,11 @@
 </template>
 <script>
 import wpapi from "wpapi";
+import $ from "cheerio";
 export default {
   methods: {
     async filter(id) {
-      console.log('filter');
+      console.log("filter");
       let wp = new wpapi({
         endpoint: "https://eathereindy.nfshost.com/wp-json/",
         username: "tylerhillwebdev",
@@ -34,28 +42,37 @@ export default {
       });
       console.log(id);
       let test = this.selected.indexOf(id);
-      console.log('test',test);
-      if(test > -1) {
-        this.selected.splice(test,1);
+      console.log("test", test);
+      if (test > -1) {
+        this.selected.splice(test, 1);
       } else {
         this.selected.push(id);
       }
-      console.log('selected',this.selected);
-      let filtered = await wp.posts().tags(this.selected).perPage(100).get();
-      console.log("filtered", filtered);
+      console.log("selected", this.selected);
+      let filteredPosts = await wp
+        .posts()
+        .tags(this.selected)
+        .categories(183)
+        .perPage(100)
+        .get();
+      let filtered = [];
+      for (let post of filteredPosts) {
+        var jstr = $("<div/>").html(post.content.rendered).text();
+        var obj = JSON.parse(jstr);
+        obj.link = post.link;
+        filtered.push(obj);
+      }
+      console.log('filtered',filtered,filtered.length);
+      this.filtered = filtered;
     },
   },
   data() {
     return {
       selected: [],
+      filtered: [],
     };
   },
   computed: {
-    filtered() {
-      let filtered = [];
-      return filtered;
-      // for (let post in this.$store.state.posts);
-    },
     facets() {
       return this.$store.state.facets;
     },
